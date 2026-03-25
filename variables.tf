6242b221d247b9d0a042d07dbc7e4c97f554f9d5
Saman Saybani <varx@Samans-MacBook-Air.local>
2026-03-25 22:45:11 +0100

    cleaning

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 # # # #VARIABLES # # # #
 
+# # Common variables
 variable "location" {
   type        = string
   description = "Location all resources"
 }
 
+# # Resource Groups
 variable "ai_project_name_rg" {
   type        = string
   description = "Name of the Resource Group for the AI project"
