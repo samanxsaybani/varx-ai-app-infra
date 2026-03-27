a382bfc40e80b946d3b3f47de838033ebd42cedd
Saman Saybani <varx@Samans-MacBook-Air.local>
2026-03-27 13:25:10 +0100

    updating model

--- infra-terraform/ai-services.tf
+++ infra-terraform/ai-services.tf
@@ -13,7 +13,7 @@ resource "azurerm_cognitive_account" "openai" {
 
 # # OpenAI Model Deployment (GPT-3.5 Turbo)
 resource "azurerm_cognitive_deployment" "openai_deployment" {
-  name                 = "gpt-5.4-nano"
+  name                 = "gpt-4"
   cognitive_account_id = azurerm_cognitive_account.openai.id
 
   model {
